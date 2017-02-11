de9ac10df5e01fc03957ed296c1215f82dd41c09
Ricardo Gaete <ricardo.gaete@latam.com>
2017-02-11 15:59:04 -0300

    Generated code

---
+++ src/main/java/com/socrates/ApiDesignExampleApplication.java
@@ -0,0 +1,12 @@
+package com.socrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesignExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesignExampleApplication.class, args);
+	}
+}
